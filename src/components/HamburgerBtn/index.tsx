502a83a601ac1469226a07fe19fa9546e499ece7
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2024-12-23 01:47:53 +0900

    cnを使用する形に変更

--- src/components/HamburgerBtn/index.tsx
+++ src/components/HamburgerBtn/index.tsx
@@ -1,20 +1,32 @@
+import { cn } from "@/libs/utils";
+
 type Props = {
     isOpen: boolean;
     handleOpen: () => void;
 }
 
+
 const HamburgerBtn: React.FC<Props> = ({ isOpen, handleOpen }) => {
     return (
         <button type="button" className="flex flex-col gap-2" onClick={handleOpen}>
-            <div className={
-                `w-8 h-0.5 bg-black transition duration-500 ease-in-out ${isOpen && "translate-y-2.5 rotate-45"}`
-            }/>
-            <div className={
-                `w-8 h-0.5 bg-black transition duration-500 ease-in-out ${isOpen && "opacity-0"}`
-            }/>
-            <div className={
-                `w-8 h-0.5 bg-black transition duration-500 ease-in-out ${isOpen && "-translate-y-2.5 -rotate-45"}`
-            }/>
+            <div
+                className={cn(
+                    "w-8 h-0.5 bg-black transition duration-500 ease-in-out",
+                    isOpen && "translate-y-2.5 rotate-45",
+                )}
+            />
+            <div 
+                className={cn(
+                    "w-8 h-0.5 bg-black transition duration-500 ease-in-out",
+                    isOpen && "opacity-0",
+                )}
+            />
+            <div
+                className={cn(
+                    "w-8 h-0.5 bg-black transition duration-500 ease-in-out",
+                    isOpen && "-translate-y-2.5 -rotate-45",
+                )}
+            />
         </button>
     );
 }
