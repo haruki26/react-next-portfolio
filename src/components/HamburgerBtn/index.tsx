208eb404a758c7d950e78d9e36920c31538af013
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2024-12-26 02:11:46 +0900

    hamburgerをダークモードに対応

--- src/components/HamburgerBtn/index.tsx
+++ src/components/HamburgerBtn/index.tsx
@@ -11,14 +11,17 @@ const HamburgerBtn: React.FC<Props> = ({ isOpen, handleOpen }) => {
         <button type="button" className="flex flex-col h-fit gap-2" onClick={handleOpen}>
             <span className={cn(
                 "w-8 h-0.5 bg-black transition duration-500 ease-in-out",
+                "dark:bg-zinc-400",
                 isOpen && "translate-y-2.5 rotate-45",
             )}/>
             <span className={cn(
                 "w-8 h-0.5 bg-black transition duration-500 ease-in-out",
+                "dark:bg-zinc-400",
                 isOpen && "opacity-0",
             )}/>
             <span className={cn(
                 "w-8 h-0.5 bg-black transition duration-500 ease-in-out",
+                "dark:bg-zinc-400",
                 isOpen && "-translate-y-2.5 -rotate-45",
             )}/>
         </button>
