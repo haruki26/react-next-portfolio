2dabc5ff69f3bc23f6a8fea032725ba8f8607b54
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-11 15:57:54 +0900

    Cardでフレームのみを提供

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,20 +4,22 @@ import clsx from "clsx";
 
 type Props = {
     children: React.ReactNode;
+    className?: string;
 }
 
-const Card: React.FC<Props> = ({ children }) => {
+const Card: React.FC<Props> = ({
+    children,
+    className = "",
+}) => {
     return (
         <div className={cn(
-            "rounded-lg w-80 h-52",
-            "bg-card/30 text-card-foreground shadow-md transition duration-300",
+            className,
+            "rounded-lg bg-card/30 text-card-foreground shadow-md transition duration-300",
             "hover:bg-card/50 hover:shadow-xl hover:scale-[1.01]",
             "dark:bg-card/40 dark:text-card-foreground shadow-zinc-400/20",
             "dark:hover:bg-card/60 dark:hover:shadow-zinc-400/40",
         )}>
-            <div className="w-full h-full overflow-hidden">
-                {children}
-            </div>
+            {children}
         </div>
     );
 }
