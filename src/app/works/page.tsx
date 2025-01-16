e218340937c38d87c6ec647b5d0963a4af81daf9
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-17 01:26:57 +0900

    worksのpage.tsxでCardStackを使用するように変更

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,7 +1,36 @@
 import { BiBookBookmark } from "react-icons/bi";
 import IconAndStr from "@/components/IconAndStr";
 import Card from "@/components/Card";
+import CardStack from "@/components/CardStack";
 
+
+const dummy = [
+    {
+        title: "準備中",
+        description: "contcfsvfsrsbsdrsrergesrfvsdrgsertgsdcvfergbvewrvsxgbrevgr",
+        link: "#"
+    },
+    {
+        title: "準備中...",
+        description: "content",
+        link: "#"
+    },
+    {
+        title: "準備中...",
+        description: "content",
+        link: "#"
+    },
+    {
+        title: "準備中...",
+        description: "contwefwerfewrgbythtyrbnmyujhrgetrhrsfefvgrteesrgent",
+        link: "#"
+    },
+    {
+        title: "準備中...",
+        description: "content",
+        link: "#"
+    },
+]
 
 const Page: React.FC = () => {
     return (
@@ -10,36 +39,7 @@ const Page: React.FC = () => {
                 <BiBookBookmark size={"3.7rem"} />
                 <span className="font-roboto text-6xl">Works</span>
             </IconAndStr>
-            <div className="grid grid-cols-3 grid-rows-1 gap-4">
-                <Card className="grid grid-rows-subgrid row-span-3 gap-2 p-3">
-                    <div className="">
-                        <span className="text-3xl">準備中</span>
-                    </div>
-                    <div className="">
-                        <span>contcfsvfs<br />rsbsd<br />rsrvgr</span>
-                        <span>content</span>
-                    </div>
-                    <span>Im pen.</span>
-                </Card>
-                <Card className="grid grid-rows-subgrid row-span-3 gap-2 p-3">
-                    <div className="">
-                        <span>準備中...</span>
-                    </div>
-                    <div className="">
-                        <span>content</span>
-                    </div>
-                    <span>Im pen.</span>
-                </Card>
-                <Card className="grid grid-rows-subgrid row-span-3 gap-2 p-3">
-                    <div className="">
-                        <span>準備中...</span>
-                    </div>
-                    <div className="">
-                        <span>content</span>
-                    </div>
-                    <span className="text-xl">Im pen.</span>
-                </Card>
-            </div>
+            <CardStack cardContents={dummy} />
         </div>
     );
 }
