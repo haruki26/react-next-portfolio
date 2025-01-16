031c5171f565ba3fc8ad9ddb7f086f985129607f
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-16 22:42:11 +0900

    Heroのダークモード時の線の色を変更

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,7 +12,7 @@ const Hero: React.FC<Props> = ({ title, subTitle }) => {
             "after:block after:mt-12 after:mx-auto after:h-px after:w-full after:bg-slate-800/70",
             "after:animate-scale-in-center after:max-w-screen-md",
             "sm:pt-24 sm:pb-28 sm:gap-12 sm:after:mt-24",
-            "dark:after:bg-yellow-200/50",
+            "dark:after:bg-yellow-400/50",
         )}>
             <h1 className="text-5xl tracking-tighter sm:text-8xl">
                 {title}
