8b33d06109d9cfef7c5edc7d18ffebfab7603e81
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-16 22:54:52 +0900

    CardStackの実装

--- src/components/CardStack/index.tsx
+++ src/components/CardStack/index.tsx
@@ -1,15 +1,32 @@
+import Link from "next/link";
 import Card from "../Card";
 
-type Props = {
-    children: React.ReactNode;
+
+type Content = {
+    title: string;
+    description: string;
+    link: string;
 }
 
-const CardStack: React.FC<Props> = ({
-    children,
-}) => {
+type Props = {
+    cardContents: Content[];
+}
+
+const CardStack: React.FC<Props> = ({ cardContents }) => {
     return (
-        <div className="grid grid-rows-3 gap-4">
-        </div>
+        <ul className="grid grid-cols-1 gap-4">
+            {cardContents.map((content, index) => (
+                <li key={index}>
+                    <Card className="grid grid-rows-subgrid row-span-3 col-span-1 gap-1">
+                        <h3 className="">{content.title}</h3>
+                        <p className="">{content.description}</p>
+                        <Link href={content.link}>
+                            <span>Read more</span>
+                        </Link>
+                    </Card>
+                </li>
+            ))}
+        </ul>
     );
 }
 
