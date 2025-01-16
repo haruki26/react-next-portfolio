da84342a8ef7933fd41aa7239e672a060d9bfa1b
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-17 01:27:43 +0900

    Cardで引数classNameを後ろへ移動

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/libs/utils";
-import clsx from "clsx";
 
 
 type Props = {
@@ -13,11 +12,11 @@ const Card: React.FC<Props> = ({
 }) => {
     return (
         <div className={cn(
-            className,
             "rounded-lg bg-card/30 text-card-foreground shadow-md transition duration-300",
             "hover:bg-card/50 hover:shadow-xl hover:scale-[1.01]",
             "dark:bg-card/40 dark:text-card-foreground shadow-zinc-400/20",
             "dark:hover:bg-card/60 dark:hover:shadow-zinc-400/40",
+            className,
         )}>
             {children}
         </div>
