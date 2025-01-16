cf801911e6f386066e05a0d53ab6124ce82ce7d7
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-17 01:26:14 +0900

    CardStackのレスポンシブ対応

--- src/components/CardStack/index.tsx
+++ src/components/CardStack/index.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Card from "../Card";
+import clsx from "clsx";
+import { cn } from "@/libs/utils";
 
 
 type Content = {
@@ -13,20 +15,29 @@ type Props = {
 }
 
 const CardStack: React.FC<Props> = ({ cardContents }) => {
+    const cl = cardContents.length;
+    const rowSpan = (cl % 2 == 0 ? cl : cl + 1) * 3;
+
     return (
-        <ul className="grid grid-cols-1 gap-4">
+        <div className={cn(
+            "grid grid-rows-9",
+            `grid-cols-1 grid-rows-[repeat(${rowSpan},minmax(0,1fr))] gap-5`,
+            `md:grid-cols-2 md:grid-rows-[repeat(${rowSpan/2},minmax(0,1fr))]`,
+        )}>
             {cardContents.map((content, index) => (
-                <li key={index}>
-                    <Card className="grid grid-rows-subgrid row-span-3 col-span-1 gap-1">
-                        <h3 className="">{content.title}</h3>
-                        <p className="">{content.description}</p>
-                        <Link href={content.link}>
-                            <span>Read more</span>
-                        </Link>
-                    </Card>
-                </li>
+                <Card key={index} className={clsx(
+                    "px-2 py-3 grid grid-rows-subgrid row-span-3 max-w-72 gap-1",
+                )}>
+                    <h3 className="font-mplus text-2xl">{content.title}</h3>
+                    <div className="flex gap-0">
+                        <p className="text-xl break-all">{content.description}</p>
+                    </div>
+                    <Link href={content.link}>
+                        <span className="text-xl">Read more</span>
+                    </Link>
+                </Card>
             ))}
-        </ul>
+        </div>
     );
 }
 
