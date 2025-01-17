f3645a8b7d3a56d3fb73d6024fa69e40d6b55e51
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-17 14:07:48 +0900

    buttonでclassnameを受け取るように変更

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,24 +3,27 @@ import { cn } from "@/libs/utils";
 
 type Props = {
     children: React.ReactNode;
+    className?: string;
 }
 
-const Button: React.FC<Props> = ({ children }) => {
+const Button: React.FC<Props> = ({ children, className }) => {
     return (
-        <button
-            type="button"
-            className={cn(
-                "h-16 w-52 bg-muted mx-auto relative",
-                "before:absolute before:top-0 before:left-0 before:w-full before:h-full",
-                "before:w-full before:h-full before:border-black before:border-[1.5px]",
-                "before:transform before:translate-x-[6px] before:-translate-y-[6px]",
-                "hover:before:translate-x-0 hover:before:translate-y-0",
-                "before:transition before:duration-300",
-                "dark:before:border-slate-300"
-            )}
-        >
-            {children}
-        </button>
+        <div className={className}>
+            <button
+                type="button"
+                className={cn(
+                    "h-16 w-52 bg-muted mx-auto relative",
+                    "before:absolute before:top-0 before:left-0 before:w-full before:h-full",
+                    "before:w-full before:h-full before:border-black before:border-[1.5px]",
+                    "before:transform before:translate-x-[6px] before:-translate-y-[6px]",
+                    "hover:before:translate-x-0 hover:before:translate-y-0",
+                    "before:transition before:duration-300",
+                    "dark:before:border-slate-300"
+                )}
+            >
+                {children}
+            </button>
+        </div>
     );
 }
 
