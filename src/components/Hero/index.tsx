5593a08887641453ccc3452a4567c6c56ca2dc80
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-17 15:27:44 +0900

    heroでsubtitleからdescribeに変更

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,25 +3,25 @@ import { cn } from "@/libs/utils";
 
 type Props = {
     children: React.ReactNode;
-    subTitle?: string | null;
+    describe?: string | null;
 }
 
 const Hero: React.FC<Props> = ({
     children,
-    subTitle = null,
+    describe = null,
 }) => {
     return (
         <div className={cn(
-            "text-center pt-10 pb-12 w-full flex flex-col gap-7 items-center font-roboto font-light tracking-tighter",
+            "text-center pt-10 pb-12 w-full flex flex-col gap-7 items-center",
             "after:block after:mt-12 after:mx-auto after:h-px after:w-full after:bg-slate-800/70",
             "after:animate-scale-in-center after:max-w-screen-md",
             "sm:pt-24 sm:pb-28 sm:gap-12 sm:after:mt-24",
             "dark:after:bg-yellow-400/50",
         )}>
             {children}
-            {subTitle && (
-                <span className="text-xl animate-fade-in-bottom sm:text-4xl">
-                    {subTitle}
+            {describe && (
+                <span className="font-mplus text-xl animate-fade-in-bottom sm:text-4xl">
+                    {describe}
                 </span>
             )}
         </div>
