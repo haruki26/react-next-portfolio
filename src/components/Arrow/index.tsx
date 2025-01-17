cbedea9071feaec353ea62283bfed3d51b1cc791
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-17 15:01:18 +0900

    Arrowのホバー時の移動距離を短縮

--- src/components/Arrow/index.tsx
+++ src/components/Arrow/index.tsx
@@ -9,8 +9,8 @@ const Arrow: React.FC<Props> = ({ children, className }) => {
     return (
         <div className={clsx(
             "after:inline-block after:w-[1.5rem] after:h-[0.4rem] after:ml-2 after:my-auto after:pr-1",
-            "after:border-solid after:border-b-[1px] after:border-r-[1px] after:border-muted-foreground",
-            "after:skew-x-[45deg] after:transition after:duration-300 hover:after:translate-x-1",
+            "after:border-solid after:border-b-[2px] after:border-r-[2px] after:border-muted",
+            "after:skew-x-[45deg] after:transition after:duration-300 hover:after:translate-x-[3px]",
             className,
         )}>
             {children}
