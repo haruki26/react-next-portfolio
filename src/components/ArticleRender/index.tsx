480c434fbbf09603603b4b1f96b3c519dc05329d
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-18 15:47:51 +0900

    記事のレンダリングを行うコンポーネントを作成

---
+++ src/components/ArticleRender/index.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import parser from "html-react-parser";
+
+
+type Props = {
+    html: string;
+}
+
+const ArticleRender: React.FC<Props> = ({ html }) => {
+    console.log(html);
+    return (
+        <article className={clsx(
+            "w-full prose break-all md:prose-lg",
+            "prose-pre:w-80 prose-pre:mx-auto xs:prose-pre:w-[480px] md:prose-pre:w-full",
+            "prose-code:overflow-x-scroll",
+        )}>
+            {parser(html)}
+        </article>
+    )
+}
+
+export default ArticleRender;
