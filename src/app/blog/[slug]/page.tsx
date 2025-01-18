4b86b760f141aee1c573b70d451dcd80f320a324
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-19 02:46:51 +0900

    blogページに公開日等を記載

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import ArticleRender from "@/components/ArticleRender";
+import DateLabel from "@/components/DateLabel";
 import { getContentsDetail } from "@/libs/microcms";
 
 type Props = {
@@ -12,11 +13,20 @@ const Page: React.FC<Props> = async ({ params }) => {
 
     return (
         <div className="mx-auto flex flex-col gap-10 font-mplus justify-center items-center max-w-screen-md">
-            <h1 className="text-3xl text-center font-bold">{data.title}</h1>
-            <span></span>
-            <p>{data.describe}</p>
+            <div className="flex flex-col gap-5">
+                <h1 className="text-3xl text-center font-bold">{data.title}</h1>
+                <div className="flex gap-4 mx-auto">
+                    <span className="tracking-tight">公開日</span>
+                    <DateLabel date={data.createdAt} />
+                </div>
+                <p>{data.describe}</p>
+            </div>
             <span className="inline-block w-full h-[0.7px] bg-slate-600 " />
             <ArticleRender html={data.article} />
+            <div className="flex gap-4 mx-auto">
+                <span className="tracking-tight">最終更新日</span>
+                <DateLabel date={data.updatedAt} />
+            </div>
         </div>
     )
 }
