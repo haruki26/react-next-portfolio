ccf6cccd7417ce1214369a24e172f2bb86fd351c
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-19 01:37:09 +0900

    時間整形の関数を作成

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
 export function cn(...inputs: ClassValue[]): string {
     return twMerge(clsx(inputs));
+}
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const formatDate = (date: string): string => {
+    return dayjs(date).tz("Asia/Tokyo").format("YYYY-MM-DD");
 }
