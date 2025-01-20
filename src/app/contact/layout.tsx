051ea437344c2756247424a9f47cdfbb341bb265
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-20 23:08:38 +0900

    Contactページを作成

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,24 @@
+import { BiBookBookmark } from "react-icons/bi";
+import Hero from "@/components/Hero";
+import IconAndStr from "@/components/IconAndStr";
+
+
+type Props = {
+    children: React.ReactNode;
+};
+
+const ContactLayout: React.FC<Props> = ({ children }) => {
+    return (
+        <div className="flex flex-col gap-4 px-5 sm:px-0">
+            <Hero describe={"お問い合わせはこちらからお願いします"} >
+                <IconAndStr className="gap-3">
+                    <BiBookBookmark size={"3.7rem"} />
+                    <h1 className="font-roboto text-6xl">Contact</h1>
+                </IconAndStr>
+            </Hero>
+            {children}
+        </div>
+    );
+};
+
+export default ContactLayout;
