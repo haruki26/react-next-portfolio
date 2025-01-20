719c6b277f9a69b4ea3091006ee06424a10507a4
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-20 22:59:28 +0900

    ナビゲーションバーのラッパを削除

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -13,7 +13,6 @@ const NavBar: React.FC = () => {
     const path = formatPath(usePathname(), true);
     
     return (
-        <>
         <div className={clsx([
             "h-16 w-full px-3 py-2",
             "bg-zinc-100/75 shadow-lg shadow-slate-800/20",
@@ -27,7 +26,6 @@ const NavBar: React.FC = () => {
                 <MediaLinks />
             </div>
         </div>
-        </>
     );
 }
 
