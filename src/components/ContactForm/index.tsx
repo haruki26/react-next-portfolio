f71cb6010d4ecd29835e72aa75a9f43830d0a35c
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-24 16:13:04 +0900

    モードとレスポンシブに対応

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,70 +1,23 @@
-"use client";
-
 import clsx from "clsx";
-import { useState } from "react";
 import Button from "../Button";
+import Input from "./Input";
+import TextArea from "./TextArea";
 
-
-type LabelProps = {
-    target: string;
-}
-
-const InputForm: React.FC<LabelProps> = ({ target }) => {
-    const [inputValue, setInputValue] = useState("");
-    const [isFocused, setIsFocused] = useState(false);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(e.target.value);
-    };
-
-    return (
-        <div className="flex flex-col gap-0">
-            <label
-                htmlFor={target.toLowerCase()}
-                className={clsx(
-                    "font-roboto text-slate-600 text-md tracking-tight",
-                    "transform transition duration-150",
-                    (inputValue || isFocused) ? "translate-y-0" : "translate-y-5",
-                )}>
-                    {target}
-            </label>
-            <input
-                type="text"
-                id={target.toLowerCase()}
-                name={target.toLowerCase()}
-                value={inputValue}
-                onChange={handleChange}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                className="px-2 w-full border-b-2 bg-inherit rounded-t border-slate-800 focus:bg-stone-300/75 focus:outline-none"
-            />
-        </div>
-    );
-};
 
 const ContactForm: React.FC = () => {
     return (
         <form className={clsx(
             "mx-auto mb-2 w-80 h-fit rounded-lg p-5 bg-zinc-100/75 dark:bg-zinc-900/75",
             "shadow-lg shadow-slate-800/20 dark:shadow-slate-100/20",
-            "",
+            "sm:w-96 md:w-[30rem]",
         )}>
-            <div className="flex flex-col gap-5">
-                <div className="flex gap-3">
-                    <InputForm target="FirstName" />
-                    <InputForm target="LastName" />
+            <div className="flex flex-col gap-7">
+                <div className="w-full flex gap-1 flex-col sm:flex-row sm:gap-3">
+                    <Input target="FirstName" />
+                    <Input target="LastName" />
                 </div>
-                <InputForm target="eMail" />
-                <div className="flex flex-col gap-0">
-                    <label htmlFor="message" className="font-roboto text-slate-600 text-md tracking-tight">
-                        Message
-                    </label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        className="px-2 w-full h-24 border-b-2 bg-inherit rounded-t border-slate-800 focus:bg-stone-300/75 focus:outline-none"
-                    />
-                </div>
+                <Input target="eMail" />
+                <TextArea target="Message" />
                 <Button type="submit" className="w-32 h-12 mx-auto mt-5 mb-3 before:translate-x-[4px] before:-translate-y-[4px]">
                     <div className="w-full h-full flex justify-center items-center">
                         <span className="font-roboto text-xl text-slate-200">Submit</span>
