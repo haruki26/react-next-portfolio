5832e4128390cf586b7ca27a33f56683eaa0ac08
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-25 00:20:13 +0900

    pageとアイコンのセットを定数として定義

---
+++ src/constans.tsx
@@ -0,0 +1,24 @@
+import { BiBookBookmark, BiBookContent, BiEnvelope, BiHome } from "react-icons/bi";
+
+export const PAGES = [
+    {
+        icon: <BiHome />,
+        text: "Home",
+        href: "/",
+    },
+    {
+        icon: <BiBookBookmark />,
+        text: "Works",
+        href: "/works",
+    },
+    {
+        icon: <BiBookContent />,
+        text: "Blog",
+        href: "/blog",
+    },
+    {
+        icon: <BiEnvelope />,
+        text: "Contact",
+        href: "/contact",
+    },
+]
