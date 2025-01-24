c9ec49df9b08ef38e5d92262bf53bdd79bd8ddb0
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-25 00:31:16 +0900

    footerのスタイル調整

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,36 +1,29 @@
 import { cn } from "@/libs/utils";
-import type { IconSettings } from "../PageLinks";
 
-import ThemeChangeBtn from "../ThemeChangeBtn";
-import TopBackBtn from "../TopBackBtn";
-import PageLinks from "../PageLinks";
+import MediaLinks from "../MediaLinks";
+import { PAGES } from "@/constans";
+import Link from "next/link";
 
 
 const Footer: React.FC = () => {
-    const iconSetting: IconSettings = {
-        size: 28,
-        className: cn(
-            "fill-white",
-        )
-    };
 
     return (
         <div className={cn(
-            "h-fit w-full px-3 py-4 bg-[#3e2723] text-[#f5f5dc] shadow-[0_-4px_9px_8px_rgba(0,0,0,0.18)]",
+            "h-fit w-full px-3 py-8 bg-[#3e2723] text-[#f5f5dc] shadow-[0_-4px_9px_8px_rgba(0,0,0,0.18)]",
             "dark:bg-[#2e2e2e] dark:shadow-[0_-2px_10px_6px_rgba(221,221,221,0.18)]",
         )}>
-            <div className="h-full flex flex-col gap-7 justify-center items-center">
-                <div className="pl-6 flex gap-10 items-center">
-                    <TopBackBtn />
-                    <ThemeChangeBtn />
+            <div className="h-full flex flex-col gap-5 justify-center items-center">
+                <div className="w-full flex gap-5 items-center justify-center">
+                    {PAGES.map((page, index) => (
+                        <Link href={page.href} key={index}>
+                            {page.icon.type({
+                                size: "2.3rem",
+                                className: "fill-slate-200",
+                            })}
+                        </Link>
+                    ))}
                 </div>
-                <div className="flex flex-col gap-2 items-center">
-                    <PageLinks
-                        icon={iconSetting}
-                        className="gap-6"
-                        horizontal
-                    />
-                </div>
+                <MediaLinks />
             </div>
         </div>
     );
--- src/components/MediaLinks/index.tsx
+++ src/components/MediaLinks/index.tsx
@@ -5,10 +5,10 @@ const MediaLinks: React.FC = () => {
     return (
         <div className="flex gap-4">
             <a href="https://www.instagram.com/yousei_81" target="_blank">
-                <IoLogoInstagram size={"2rem"} />
+                <IoLogoInstagram size={"2.3rem"} className="fill-slate-200 pt-[3px]" />
             </a>
             <a href="https://github.com/haruki26" target="_blank">
-                <IoLogoGithub size={"2rem"} />
+                <IoLogoGithub size={"2.3rem"} className="fill-slate-200" />
             </a>
         </div>
     );
