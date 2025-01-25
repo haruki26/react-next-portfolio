0377857a6ccbeb6ad627ebcfe536ded1ea049ea9
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-25 19:09:05 +0900

    フォームを再構築

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,30 +1,82 @@
+"use client";
+
 import clsx from "clsx";
 import Button from "../Button";
 import Input from "./Input";
 import TextArea from "./TextArea";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { contactFormSchema, ContactFormSchema } from "@/validation/schema/ContactFormSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createContact } from "@/validation/actoin/contact";
+import ErrorMessage from "./ErrorMessage";
+import React, { useState } from "react";
+import Success from "./Success";
+import Link from "next/link";
+
+type Status = {
+    state: "pending" | "success" | "error";
+    message: string;
+}
 
 
 const ContactForm: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isLoading, isSubmitSuccessful },
+        setError,
+        reset,
+    } = useForm<ContactFormSchema>(
+        {
+            mode: "onSubmit",
+            resolver: zodResolver(contactFormSchema),
+        }
+    );
+
+    const onSubmit: SubmitHandler<ContactFormSchema> = async (data) => {
+        const result = await createContact({status: "pending", message: ""}, data);
+        if (result.status === "error") {
+            setError("root", {
+                type: "server",
+                message: result.message,
+            });
+        }
+        else {
+            reset();
+        }
+    };
+
     return (
-        <form className={clsx(
-            "mx-auto mb-2 w-80 h-fit rounded-lg p-5 bg-zinc-100/75 dark:bg-zinc-900/75",
-            "shadow-lg shadow-slate-800/20 dark:shadow-slate-100/20",
-            "sm:w-96 md:w-[30rem]",
-        )}>
-            <div className="flex flex-col gap-7">
-                <div className="w-full flex gap-1 flex-col sm:flex-row sm:gap-3">
-                    <Input target="FirstName" />
-                    <Input target="LastName" />
-                </div>
-                <Input target="eMail" />
-                <TextArea target="Message" />
-                <Button type="submit" className="w-32 h-12 mx-auto mt-5 mb-3 before:translate-x-[4px] before:-translate-y-[4px]">
-                    <div className="w-full h-full flex justify-center items-center">
-                        <span className="font-roboto text-xl text-slate-200">Submit</span>
+        isSubmitSuccessful
+        ? (
+            <Success />
+        ) : (
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className={clsx(
+                    "mx-auto mb-2 w-80 h-fit rounded-lg p-5 bg-zinc-100/75 dark:bg-zinc-900/75",
+                    "shadow-lg shadow-slate-800/20 dark:shadow-slate-100/20",
+                    "sm:w-96 md:w-[30rem]",
+                )}
+            >
+                <div className="flex flex-col gap-4">
+                    <div className="w-full flex gap-1 flex-col sm:flex-row sm:gap-3">
+                        <Input register={register("firstName")} fieldError={errors.firstName} />
+                        <Input register={register("lastName")} fieldError={errors.lastName} />
                     </div>
-                </Button>
-            </div>
-        </form>
+                    <Input register={register("email")} fieldError={errors.email} />
+                    <TextArea register={register("message")} fieldError={errors.message} />
+                    <Button type="submit" className="w-32 h-12 mx-auto mt-5 mb-3 before:translate-x-[4px] before:-translate-y-[4px]">
+                        <div className="w-full h-full flex justify-center items-center">
+                            <span className="font-roboto text-xl text-slate-200">Submit</span>
+                        </div>
+                    </Button>
+                </div>
+                <ErrorMessage message={
+                    errors.root?.message ? errors.root.message : null
+                }/>
+            </form>
+        )
     );
 };
 
