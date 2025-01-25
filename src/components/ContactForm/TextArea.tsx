f184cfc46cb3a4fee807c83e95148ce8059a7cc4
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-25 18:51:14 +0900

    フォームの要素でregisterを受け取るように変更

--- src/components/ContactForm/TextArea.tsx
+++ src/components/ContactForm/TextArea.tsx
@@ -1,46 +1,60 @@
 "use client";
 
+import { ContactFormSchema } from "@/validation/schema/ContactFormSchema";
 import clsx from "clsx";
-import { useState } from "react";
+import React, { useState } from "react";
+import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+import ErrorMessage from "./ErrorMessage";
 
 type Props = {
-    target: string;
+    register: UseFormRegisterReturn<keyof ContactFormSchema>;
+    fieldError?: FieldError;
 }
 
-const TextArea: React.FC<Props> = ({ target }) => {
+const TextArea: React.FC<Props> = ({ register, fieldError }) => {
     const [inputValue, setInputValue] = useState("");
     const [isFocused, setIsFocused] = useState(false);
 
+    const { onChange, onBlur, name, ref } = register;
+
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        onChange(e);
         setInputValue(e.target.value);
     };
+
+    const handleBlur = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        onBlur(e);
+        setIsFocused(false);
+    }
 
     return (
         <div className="flex flex-col gap-0">
             <label
-                htmlFor={target.toLowerCase()}
+                htmlFor={name}
                 className={clsx(
-                    "font-roboto text-slate-800 text-md tracking-tight dark:text-slate-300",
-                    "transform transition-transform duration-150",
+                    "font-roboto  text-md tracking-tight",
+                    "transform transition duration-150",
                     (inputValue || isFocused)
                     ? "translate-y-0 text-slate-800 dark:text-slate-300"
                     : "translate-y-5 text-slate-500 dark:text-slate-500",
                 )}>
-                    {target}
+                    {name}
             </label>
             <textarea
-                id={target.toLowerCase()}
-                name={target.toLowerCase()}
+                id={name}
+                ref={ref}
+                name={name}
                 value={inputValue}
                 onChange={handleChange}
                 onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
+                onBlur={handleBlur}
                 className={clsx(
                     "px-2 w-full h-24 border-b-2 bg-inherit border-slate-800 dark:border-slate-300",
                     "focus:bg-stone-300/75 focus:outline-none dark:focus:bg-stone-700/75 focus:border-y-2",
                     "md:h-40"
                 )}
             />
+            <ErrorMessage message={fieldError?.message} />
         </div>
     );
 };
