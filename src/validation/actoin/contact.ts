fbf30188fdd7a310d4ac05f049b31cc72e2d7283
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-25 18:49:36 +0900

    送信用の処理を作成

---
+++ src/validation/actoin/contact.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { ContactFormSchema } from "../schema/ContactFormSchema";
+
+type Return = {
+    status: "success" | "error";
+    message: string;
+}
+
+export const createContact = async (
+    _prevState: unknown,
+    formData: ContactFormSchema
+): Promise<Return> => {
+    const result = await fetch(
+        `https://api.hsforms.com/submissions/v3/integration/submit/${process.env.HUBSPOT_PORTAL_ID}/${process.env.HUBSPOT_FORM_ID}`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                fields: [
+                    {
+                        objectTypeId: "0-1",
+                        name: "firstname",
+                        value: formData.firstName,
+                    },
+                    {
+                        objectTypeId: "0-1",
+                        name: "lastname",
+                        value: formData.lastName,
+                    },
+                    {
+                        objectTypeId: "0-1",
+                        name: "email",
+                        value: formData.email,
+                    },
+                    {
+                        objectTypeId: "0-1",
+                        name: "message",
+                        value: formData.message,
+                    },
+                ],
+            }),
+        }
+    )
+
+    try {
+        await result.json();
+    } catch (error) {
+        return {
+            status: "error",
+            message: "お問い合わせに失敗しました",
+        }
+    }
+    return {
+        status: "success",
+        message: "お問い合わせが完了しました",
+    }
+};
