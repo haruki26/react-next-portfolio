9745c6293893ab9ea47379a55f00973ca034d75d
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-26 03:19:33 +0900

    worksに記事詳細ページを作成

---
+++ src/app/works/[slug]/page.tsx
@@ -0,0 +1,35 @@
+import { notFound } from "next/navigation";
+
+import { getContentsDetail } from "@/libs/microcms";
+import type { AsyncPage } from "@/types";
+import Article from "@/components/Article";
+import BackBtn from "@/components/Article/BackBtn";
+
+
+type Props = {
+    params: {
+        slug: string;
+    };
+};
+
+const Page: AsyncPage<Props> = async ({ params }) => {
+    const data = await getContentsDetail("works", params.slug).catch(notFound);
+
+    return (
+        <div className="flex flex-col items-center gap-10">
+            <Article
+                title={data.title}
+                describe={data.describe}
+                html={data.article}
+                date={{
+                    createdAt: data.createdAt,
+                    updatedAt: data.updatedAt,
+                }}
+            />
+            <BackBtn href="works" />
+        </div>
+        
+    );
+};
+
+export default Page;
