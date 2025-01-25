3a71c58f02e5f1abca27777d4c5c789c4b223899
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-26 03:17:44 +0900

    hrefを受け取るように変更

--- src/components/Article/BackBtn.tsx
+++ src/components/Article/BackBtn.tsx
@@ -2,15 +2,19 @@
 
 import Link from "next/link";
 import { toTop } from "@/libs/utils";
+import type { EndPoint } from "@/libs/microcms";
 import Button from "../Button";
 
+type Props = {
+    href: EndPoint;
+}
 
-const BackBtn: React.FC = () => {
+const BackBtn: React.FC<Props> = ({ href }) => {
     return (
         <Button>
             <Link
                 onClick={() => toTop("instant")}
-                href="/blog"
+                href={`/${href}`}
                 className="w-full h-full flex items-center justify-center"
             >
                 <span className="font-mplus text-xl text-muted-foreground font-light tracking-tighter">
