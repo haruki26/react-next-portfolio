397ff6afa71a033d1133353dc0e5f50a9f7ad296
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-25 23:41:25 +0900

    ローディングのマーク作成

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+
+const commonStyle = "inline absolute w-[8px] h-[8px] bg-white rounded-full animate-switch-scaling"
+
+const Loading: React.FC = () => {
+    return (
+        <div className="relative w-[40px] h-[40px] m-auto">
+            <span className={clsx(commonStyle, "top-0 left-[16px]")} />
+            <span className={clsx(commonStyle, "top-[5.7px] left-[26.5px] animation-delay-125")} />
+            <span className={clsx(commonStyle, "top-[16px] left-[32px] animation-delay-250")} />
+            <span className={clsx(commonStyle, "top-[26.14px] left-[26.5px] animation-delay-375")} />
+            <span className={clsx(commonStyle, "top-[32px] left-[16px] animation-delay-500")} />
+            <span className={clsx(commonStyle, "top-[26.14px] left-[5px] animation-delay-625")} />
+            <span className={clsx(commonStyle, "top-[16px] left-0 animation-delay-750")} />
+            <span className={clsx(commonStyle, "top-[5.7px] left-[5px] animation-delay-825")} />
+        </div>
+    );
+}
+
+export default Loading;
