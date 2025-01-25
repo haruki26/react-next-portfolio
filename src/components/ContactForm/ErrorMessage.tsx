e529270578a9352e2ad1dc106759b13b262a6568
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-25 18:50:16 +0900

    エラーメッセージの表示ラベルを作成

---
+++ src/components/ContactForm/ErrorMessage.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { IoInformationCircleOutline } from "react-icons/io5";
+
+
+type Props = {
+    message?: string;
+}
+
+const ErrorMessage: React.FC<Props> = ({ message }) => {
+    const { resolvedTheme } = useTheme();
+    return message ? (
+        <div className="flex items-center gap-[0.12rem] pl-1 pt-px">
+            <IoInformationCircleOutline
+                size={20}
+                color={resolvedTheme === "dark" ? "#f87171" : "#ef4444"}
+                className="fill-red-500 dark:fill-red-400" />
+            <span className="text-red-500 text-sm font-mplus tracking-tight dark:text-red-400">{message}</span>
+        </div>
+    ) : (
+        <div className="h-5" />
+    );
+};
+
+export default ErrorMessage;
