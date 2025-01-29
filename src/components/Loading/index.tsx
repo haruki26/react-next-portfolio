9d4253bdd334aa33dadaf61c50f22cd668c86799
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-30 00:54:40 +0900

    アニメーション名の修正

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -12,7 +12,7 @@ const Loading: React.FC = () => {
             <span className={clsx(commonStyle, "top-[32px] left-[16px] animation-delay-500")} />
             <span className={clsx(commonStyle, "top-[26.14px] left-[5px] animation-delay-625")} />
             <span className={clsx(commonStyle, "top-[16px] left-0 animation-delay-750")} />
-            <span className={clsx(commonStyle, "top-[5.7px] left-[5px] animation-delay-825")} />
+            <span className={clsx(commonStyle, "top-[5.7px] left-[5px] animation-delay-875")} />
         </div>
     );
 }
