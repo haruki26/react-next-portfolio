1ffe2a7cba982e7861858d9328d544d1f667496d
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-30 03:05:04 +0900

    ライトモード時のカードの色を変更

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -12,8 +12,8 @@ const Card: React.FC<Props> = ({
 }) => {
     return (
         <div className={cn(
-            "rounded-lg bg-card/30 text-card-foreground shadow-md transition duration-300",
-            "hover:bg-card/50 hover:shadow-xl hover:scale-[1.01]",
+            "rounded-lg bg-card/50 text-card-foreground shadow-md transition duration-300",
+            "hover:bg-card/70 hover:shadow-xl hover:scale-[1.01]",
             "dark:bg-card/40 dark:text-card-foreground shadow-zinc-400/20",
             "dark:hover:bg-card/60 dark:hover:shadow-zinc-400/40",
             className,
