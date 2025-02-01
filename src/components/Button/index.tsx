f9b75d2b0c2985f27e443b3b90358a2f53106f06
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-01 10:29:13 +0900

    ボタンデザインの変更

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -18,13 +18,19 @@ const Button: React.FC<Props> = ({
         <button
             type={type}
             className={cn(
-                "h-16 w-52 bg-muted relative",
+                "h-16 w-52 relative bg-muted/90 dark:bg-muted",
                 "before:w-full before:h-full before:absolute before:top-0 before:left-0",
                 "before:border-black before:border-[1.5px]",
-                "before:transform before:translate-x-[6px] before:-translate-y-[6px]",
+                "before:transform before:translate-x-[3px] before:-translate-y-[3px]",
                 "hover:before:translate-x-0 hover:before:translate-y-0",
                 "before:transition before:duration-300",
                 "dark:before:border-slate-300",
+                "after:w-full after:h-full after:absolute after:top-0 after:left-0",
+                "after:border-black after:border-[1.5px]",
+                "after:transform after:translate-x-[-3px] after:-translate-y-[-3px]",
+                "hover:after:translate-x-0 hover:after:translate-y-0",
+                "after:transition after:duration-300",
+                "dark:after:border-slate-300",
                 className
             )}
             onClick={onClick}
