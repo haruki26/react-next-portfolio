d51338ebb6dff7871392b0c93bf96003a20e85f1
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-08 14:12:14 +0900

    padding値の調整

--- src/components/CardStack/index.tsx
+++ src/components/CardStack/index.tsx
@@ -25,7 +25,7 @@ const CardStack: React.FC<Props> = ({ cardContents }) => {
             `md:grid-cols-2 md:grid-rows-[repeat(${mdRowSpan},minmax(0,1fr))]`,
         )}>
             {cardContents.map((content, index) => (
-                <Card key={index} className="p-5 grid grid-rows-subgrid row-span-3 max-w-72 gap-4">
+                <Card key={index} className="p-4 grid grid-rows-subgrid row-span-3 max-w-72 gap-4">
                     <h3 className="font-mplus text-2xl">{content.title}</h3>
                     <div className="flex gap-0">
                         <p className="text-xl break-all">{content.describe}</p>
